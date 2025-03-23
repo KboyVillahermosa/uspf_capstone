d112665f6abe53d9191f814facdcdf00b1a089dc
Kboy <villahermosafrancisco6@gmail.com>
2025-03-23 18:18:32 +0800

    added in navbar

--- project_archive/resources/views/layouts/navigation.blade.php
+++ project_archive/resources/views/layouts/navigation.blade.php
@@ -13,6 +13,10 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    <x-nav-link :href="route('upload')" :active="request()->routeIs('upload')">
+                        {{ __('Upload') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -67,6 +71,10 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('upload')" :active="request()->routeIs('upload')">
+                {{ __('Upload') }}
             </x-responsive-nav-link>
         </div>
 
